740abd2ca2694285d0c3f4d2d04854a6b96b9845
Tommaso Papini <tommaso.papini@cern.ch>
2013-11-12 14:38:27 +0100

    [IMP] Added parameters * Added parameters to the fabfile functions * indico_initial_setup now fully automatic

--- fabfile.py
+++ fabfile.py
@@ -1,111 +1,212 @@
 from fabric.api import *
-from fabric.contrib.files import append, sed
+from fabric.contrib.files import sed
 from fabric.operations import put, run
+import os
 
 env.user_name = 'root'
-env.host_name = 'localhost'
-env.hosts = ['%s@%s:2222' % (env.user_name, env.host_name)]
-env.config_dir = 'config/'
-env.img_dir = 'img/'
-env.indico_conf_dir = '/opt/indico/etc/'
-env.httpd_conf_dir = '/etc/httpd/conf/'
-env.httpd_confd_dir = '/etc/httpd/conf.d/'
-env.iptables_dir = '/etc/sysconfig/'
-env.ssl_dir = '/etc/ssl/'
+env.host_name = '127.0.0.1'
+env.host_port = '2222'
+env.http_port = '8000'
+env.https_port = '8443'
+env.hosts = ["{0}@{1}:{2}".format(env.user_name, env.host_name, env.host_port)]
 
-# Dependencies installation
+env.config_dir = 'config'
+env.img_dir = 'img'
+env.img_name = 'SLC6.qcow2'
+env.img_path = os.path.join(env.img_dir, env.img_name)
+env.vd_name = 'init.iso'
+env.vd_path = os.path.join(env.img_dir, env.vd_name)
+
+
+env.indico_inst_dir = '/opt/indico'
+env.db_inst_dirname = 'db'
+env.db_inst_dir = os.path.join(env.indico_inst_dir, env.db_inst_dirname)
+
+env.indico_conf_dirname = 'etc'
+env.indico_conf_dir = os.path.join(env.indico_inst_dir, env.indico_conf_dirname)
+env.httpd_conf_dir = '/etc/httpd/conf'
+env.httpd_confd_dir = '/etc/httpd/conf.d'
+env.iptables_dir = '/etc/sysconfig'
+env.ssl_dir = '/etc/ssl'
+
+env.virtualization_cmd = 'kvm'
+
+def _args_setup(host_name=env.host_name, host_port=env.host_port, config_dir=env.config_dir,
+                img_path=env.img_path, vd_path=env.vd_path, indico_inst_dir=env.indico_inst_dir,
+                db_inst_dir=None, virtualization_cmd=env.virtualization_cmd, http_port=env.http_port,
+                https_port=env.https_port):
+    env.host_name = host_name
+    env.host_port = host_port
+    env.hosts[0] = "{0}@{1}:{2}".format(env.user_name, env.host_name, env.host_port)
+    env.config_dir = config_dir
+    env.img_path = img_path
+    env.vd_path = vd_path
+    env.indico_inst_dir = indico_inst_dir
+    env.indico_conf_dir = os.path.join(env.indico_inst_dir, env.indico_conf_dirname)
+    if db_inst_dir is None:
+        env.db_inst_dir = os.path.join(env.indico_inst_dir, env.db_inst_dirname)
+    else:
+        env.db_inst_dir = db_inst_dir
+    env.virtualization_cmd = virtualization_cmd
+    env.http_port = http_port
+    env.https_port = https_port
+    
+
 def dependencies_inst():
-	run('yum -y install python-devel.x86_64 gcc.x86_64 httpd.x86_64 mod_wsgi.x86_64 \
-	    python-reportlab.x86_64 python-imaging.x86_64 python-lxml.x86_64 mod_ssl.x86_64')
-	run('easy_install ZODB3==3.10.5 zc.queue==1.3')
+    """
+    Dependencies installation
+    """
 
-# Indico installation and first setup
-def indico_inst():
-	run('easy_install indico')
-	run('indico_initial_setup')
+    run('yum -y install python-devel.x86_64 gcc.x86_64 httpd.x86_64 mod_wsgi.x86_64 \
+        python-reportlab.x86_64 python-imaging.x86_64 python-lxml.x86_64 mod_ssl.x86_64')
+    run('easy_install ZODB3==3.10.5 zc.queue==1.3')
+
+def indico_inst(indico_inst_dir=env.indico_inst_dir, db_inst_dir=None):
+    """
+    Indico installation and first setup
+    """
+
+    _args_setup(indico_inst_dir=indico_inst_dir, db_inst_dir=db_inst_dir)
+
+    run('easy_install indico')
+    run("echo -e \"{0}\nc\ny\n{1}\" | indico_initial_setup"\
+        .format(env.indico_inst_dir, env.db_inst_dir))
 
 
-# Configure Indico and the database
-def config_indico():
-	# Moving the Indico Apache .conf file
-	put('%sindico.conf' % env.config_dir, env.httpd_confd_dir)
+def config_indico(host_name=env.host_name, config_dir=env.config_dir,
+                  indico_inst_dir=env.indico_inst_dir, http_port=env.http_port,
+                  https_port=env.https_port):
+    """
+    Configure Indico and the database
+    """
 
-	# Self-generating an ssl certificate
-	run('mkdir -p %scerts' % env.ssl_dir)
-	run('mkdir -p %sprivate' % env.ssl_dir)
-	run('openssl req -new -x509 -nodes -out %scerts/self-gen.pem \
-	    -keyout %sprivate/self-gen.key -days 3650 -subj \'/CN=%s\'' %
-	    (env.ssl_dir, env.ssl_dir, env.host_name))
+    _args_setup(host_name=host_name, config_dir=config_dir, indico_inst_dir=indico_inst_dir, \
+                http_port=http_port, https_port=https_port)
 
-	# Adding a ServerName in httpd.conf
-	sed('%shttpd.conf' % env.httpd_conf_dir, \
-		'#ServerName www.example.com:80', \
-		'ServerName %s' % env.host_name)
+    # Moving the Indico Apache .conf file
+    put(os.path.join(env.config_dir, 'indico.conf'), env.httpd_confd_dir)
 
-	# Adding the ports 80 and 443 to the iptables
-	run('sed "11i\-A INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT" \
-		%siptables > %stemp' % (env.iptables_dir, env.iptables_dir))
-	run('mv -f %stemp %siptables' % (env.iptables_dir, env.iptables_dir))
-	run('sed "12i\-A INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT" \
-		%siptables > %stemp' % (env.iptables_dir, env.iptables_dir))
-	run('mv -f %stemp %siptables' % (env.iptables_dir, env.iptables_dir))
-	run('service iptables restart')
+    # Self-generating an ssl certificate
+    run("mkdir -p {0}".format(os.path.join(env.ssl_dir, 'certs')))
+    run("mkdir -p {0}".format(os.path.join(env.ssl_dir, 'private')))
+    run("openssl req -new -x509 -nodes -out {0} -keyout {1} -days 3650 -subj \'/CN={2}\'" \
+        .format (os.path.join(env.ssl_dir, 'certs/self-gen.pem'), \
+                 os.path.join(env.ssl_dir, 'private/self-gen.key'), \
+                 env.host_name))
 
-	# Adding the corresponding ports to indico.conf
-	sed('%sindico.conf' % env.indico_conf_dir, \
-		'BaseURL              = \"http://localhost/indico\"', \
-		'BaseURL              = \"http://%s:8000/indico\"' % env.host_name)
-	sed('%sindico.conf' % env.indico_conf_dir, \
-		'BaseSecureURL        = \"https://localhost/indico\"', \
-		'BaseSecureURL        = \"https://%s:8443/indico\"' % env.host_name)
-	sed('%sindico.conf' % env.indico_conf_dir, \
-		'#   LoginURL             = \"\"', \
-		'LoginURL             = \"https://%s:8443/indico/signIn.py\"' % env.host_name)
+    # Adding a ServerName in httpd.conf
+    sed(os.path.join(env.httpd_conf_dir, 'httpd.conf'), \
+        '#ServerName www.example.com:80', \
+        "ServerName {0}".format(env.host_name))
 
-	# Modifying the ssl.conf file
-	put('%sssl.conf' % env.config_dir, env.httpd_confd_dir)
+    # Adding the ports 80 and 443 to the iptables
+    run("sed \"11i\\-A INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT\" {0} > {1}" \
+        .format(os.path.join(env.iptables_dir, 'iptables'), os.path.join(env.iptables_dir, 'temp')))
+    run("mv -f {0} {1}"\
+        .format(os.path.join(env.iptables_dir, 'temp'), os.path.join(env.iptables_dir, 'iptables')))
+    run("sed \"12i\\-A INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT\" {0} > {1}" \
+        .format(os.path.join(env.iptables_dir, 'iptables'), os.path.join(env.iptables_dir, 'temp')))
+    run("mv -f {0} {1}"\
+        .format(os.path.join(env.iptables_dir, 'temp'), os.path.join(env.iptables_dir, 'iptables')))
+    run('service iptables restart')
 
-# Deploy Indico into the VM
-def deploy():
-	dependencies_inst()
-	indico_inst()
-	config_indico()
+    # Adding the corresponding ports to indico.conf
+    sed(os.path.join(env.indico_conf_dir, 'indico.conf'), \
+        'BaseURL              = \"http://localhost/indico\"', \
+        "BaseURL              = \"http://{0}:{1}/indico\"" \
+        .format(env.host_name, env.http_port))
+    sed(os.path.join(env.indico_conf_dir, 'indico.conf'), \
+        'BaseSecureURL        = \"https://localhost/indico\"', \
+        "BaseSecureURL        = \"https://{0}:{1}/indico\"" \
+        .format(env.host_name, env.https_port))
+    sed(os.path.join(env.indico_conf_dir, 'indico.conf'), \
+        '#   LoginURL             = \"\"', \
+        "LoginURL             = \"https://{0}:{1}/indico/signIn.py\"" \
+        .format(env.host_name, env.https_port))
 
-# Start the database
-def start_db():
-	run('zdaemon -C %szdctl.conf start' % env.indico_conf_dir)
+    # Modifying the ssl.conf file
+    put(os.path.join(env.config_dir, 'ssl.conf'), env.httpd_confd_dir)
 
-# Disable SELinux
+def deploy(host_name=env.host_name, config_dir=env.config_dir, 
+           indico_inst_dir=env.indico_inst_dir, db_inst_dir=None,
+           http_port=env.http_port, https_port=env.https_port):
+    """
+    Deploy Indico into the VM
+    """
+
+    dependencies_inst()
+    indico_inst(indico_inst_dir, db_inst_dir)
+    config_indico(host_name, config_dir, indico_inst_dir, http_port, https_port)
+
+def start_db(indico_inst_dir=env.indico_inst_dir):
+    """
+    Start the database
+    """
+
+    _args_setup(indico_inst_dir=indico_inst_dir)
+
+    run("zdaemon -C {0} start".format(os.path.join(env.indico_conf_dir, 'zdctl.conf')))
+
 def disable_sel():
-	run('echo 0 >/selinux/enforce')
+    """
+    Disable SELinux
+    """
 
-# Start Apache
+    run('echo 0 >/selinux/enforce')
+
 def start_httpd():
-	run('service httpd start')
+    """
+    Start Apache
+    """
 
-# Start Indico
-def start():
-	start_db()
-	disable_sel()
-	start_httpd()
+    run('service httpd start')
 
-# Run the Virtual Machine
-def run_vm():
-	local('kvm -m 256 -redir tcp:2222::22 -redir tcp:8000::80 -redir tcp:8443::443 \
-		  -net nic -net user, -drive file=%sSLC6.qcow2,if=virtio -drive file=%sinit.iso,if=virtio' %
-		  (env.img_dir, env.img_dir))
+def start(indico_inst_dir=env.indico_inst_dir):
+    """
+    Start Indico
+    """
 
-# Configure Cloud-Init files
-def config_cloud_init():
-	print("Building virtual drive...")
-	local('genisoimage -output %sinit.iso -volid cidata -joliet -rock %suser-data %smeta-data' %
-		(env.img_dir, env.config_dir, env.config_dir))
-	print("Virtual drive built!")
+    start_db(indico_inst_dir)
+    disable_sel()
+    start_httpd()
 
-# Install and run Indico
-def prepare_vm():
-	config_cloud_init()
-	run_vm()
-	# something that makes the fabfile wait till the bootup is complete
-	deploy()
-	start()
+def run_vm(host_port=env.host_port, img_path=env.img_path,
+           vd_path=env.vd_path, virtualization_cmd=env.virtualization_cmd,
+           http_port=env.http_port, https_port=env.https_port):
+    """
+    Run the Virtual Machine
+    """
+
+    _args_setup(host_port=host_port, img_path=img_path, \
+                vd_path=vd_path, virtualization_cmd=virtualization_cmd, \
+                http_port=http_port, https_port=https_port)
+
+    local("{0} -m 256 -redir tcp:{1}::22 -redir tcp:{2}::80 -redir tcp:{3}::443 \
+          -net nic -net user, -drive file={4},if=virtio -drive file={5},if=virtio" \
+          .format(env.virtualization_cmd, env.host_port, env.http_port, \
+                  env.https_port, env.img_path, env.vd_path))
+
+def config_cloud_init(config_dir=env.config_dir, vd_path=env.vd_path):
+    """
+    Configure Cloud-Init files
+    """
+
+    _args_setup(config_dir=config_dir, vd_path=vd_path)
+
+    local("mkisofs -output {0} -volid cidata -joliet -rock {1} {2}" \
+          .format(env.vd_path, os.path.join(env.config_dir, 'user-data'), \
+                  os.path.join(env.config_dir, 'meta-data')))
+
+def set_up_vm(host_name=env.host_name, host_port=env.host_port, config_dir=env.config_dir,
+              img_path=env.img_path, vd_path=env.vd_path, indico_inst_dir=env.indico_inst_dir,
+              db_inst_dir=None, virtualization_cmd=env.virtualization_cmd, http_port=env.http_port,
+              https_port=env.https_port):
+    """
+    Install and run Indico
+    """
+
+    config_cloud_init(config_dir, vd_path)
+    run_vm(host_port, img_path, vd_path, virtualization_cmd, http_port, https_port)
+    # something that makes the fabfile wait till the bootup is complete
+    deploy(host_name, config_dir, indico_inst_dir, db_inst_dir, http_port, https_port)
+    start(indico_inst_dir)
