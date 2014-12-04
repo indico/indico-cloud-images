bf23ae2d475737d6c47901d42afd90c031a76002
Pedro Ferreira <pedro.ferreira@cern.ch>
2014-12-04 16:52:16 +0100

    Make gen-user-data script more usable

--- cloud-init/gen-user-data.py
+++ cloud-init/gen-user-data.py
@@ -8,15 +8,12 @@ except NameError:
 import argparse
 import os
 
-from fabric.colors import green
+from fabric.colors import cyan, green
 from yaml import dump, load
 
-parser = argparse.ArgumentParser(description='Deploy Indico on the cloud.')
-args = parser.parse_args()
 
 conf_dir = '../conf'
 tpl_dir = '../tpl'
-vrule = '---------------------------------------------------'
 
 
 def _yes_no_input(message, default):
@@ -48,95 +45,89 @@ def _add_tabs(old_content):
 
 
 def config():
-    if _yes_no_input('Do you want to use a configuration file', 'n'):
-        conf_path = _input_default('Specify the configuration file path', 'gen-user-data.conf')
-        with open(conf_path, 'r') as f:
-            conf_dict = load(f)
+    indico_inst_dir = _input_default('Insert the Indico installation directory path', '/opt/indico')
+    db_inst_dir = _input_default('Insert the Indico DB installation directory path', '/opt/indico/db')
+
+    httpd_conf_dir = _input_default('Insert the Apache conf directory', '/etc/httpd/conf')
+    httpd_confd_dir = _input_default('Insert the Apache confd directory', '/etc/httpd/conf.d')
+
+    ssl_certs_dir = _input_default('Insert the SSL cert directory', '/etc/ssl/certs')
+    ssl_private_dir = _input_default('Insert the SSL private directory', '/etc/ssl/private')
+    load_ssl = _yes_no_input('Do you want to load a personal SSL certificate', 'n')
+    if load_ssl:
+        pem_source = _input_default('Insert the source (local) path to the .pem file', 'ssl/certs/ssl-cert-snakeoil.pem')
+        key_source = _input_default('Insert the source (local) path to the .key file', 'ssl/private/ssl-cert-snakeoil.key')
     else:
-        indico_inst_dir = _input_default('Insert the Indico installation directory path', '/opt/indico')
-        db_inst_dir = _input_default('Insert the Indico DB installation directory path', '/opt/indico/db')
+        pem_source = 'self-gen.pem'
+        key_source = 'self-gen.key'
 
-        httpd_conf_dir = _input_default('Insert the Apache conf directory', '/etc/httpd/conf')
-        httpd_confd_dir = _input_default('Insert the Apache confd directory', '/etc/httpd/conf.d')
+    http_port = _input_default('Insert the http port', '80')
+    https_port = _input_default('Insert the https port', '443')
+    host_name = input('Insert the hostname: ')
 
-        ssl_certs_dir = _input_default('Insert the SSL cert directory', '/etc/ssl/certs')
-        ssl_private_dir = _input_default('Insert the SSL private directory', '/etc/ssl/private')
-        load_ssl = _yes_no_input('Do you want to load a personal SSL certificate', 'n')
-        if load_ssl:
-            pem_source = _input_default('Insert the source (local) path to the .pem file', 'ssl/certs/ssl-cert-snakeoil.pem')
-            key_source = _input_default('Insert the source (local) path to the .key file', 'ssl/private/ssl-cert-snakeoil.key')
-        else:
-            pem_source = 'self-gen.pem'
-            key_source = 'self-gen.key'
+    iptables_path = _input_default('Insert the iptables path', '/etc/sysconfig/iptables')
 
-        http_port = _input_default('Insert the http port', '80')
-        https_port = _input_default('Insert the https port', '443')
-        host_name = input('Insert the hostname: ')
+    redis_host = _input_default('Insert the Redis hostname', 'localhost')
+    redis_port = _input_default('Insert the Redis port', '6379')
+    redis_pswd = input('Insert the Redis password: ')
 
-        iptables_path = _input_default('Insert the iptables path', '/etc/sysconfig/iptables')
+    postfix = _yes_no_input('Do you want to use Postfix as mail server', 'y')
+    if postfix:
+        smtp_server_name = 'localhost'
+    else:
+        smtp_server_name = _input_default('Insert the SMTP server name', 'localhost')
+    smtp_server_port = _input_default('Insert the SMTP server port', '25')
+    smtp_login = input('Insert the SMTP login: ')
+    smtp_pswd = input('Insert the SMTP password: ')
 
-        redis_host = _input_default('Insert the Redis hostname', 'localhost')
-        redis_port = _input_default('Insert the Redis port', '6379')
-        redis_pswd = input('Insert the Redis password: ')
+    yum_repos_dir = _input_default('Insert the YUM repositories directory', '/etc/yum.repos.d')
+    puias_priority = _input_default('Insert the priority for the puias-unsupported repository', '19')
 
-        postfix = _yes_no_input('Do you want to use Postfix as mail server', 'y')
-        if postfix:
-            smtp_server_name = 'localhost'
-        else:
-            smtp_server_name = _input_default('Insert the SMTP server name', 'localhost')
-        smtp_server_port = _input_default('Insert the SMTP server port', '25')
-        smtp_login = input('Insert the SMTP login: ')
-        smtp_pswd = input('Insert the SMTP password: ')
+    conf_dict = {
+        'indico_inst_dir': indico_inst_dir,
+        'db_inst_dir': db_inst_dir,
+        'httpd_conf_dir': httpd_conf_dir,
+        'httpd_confd_dir': httpd_confd_dir,
+        'ssl_certs_dir': ssl_certs_dir,
+        'ssl_private_dir': ssl_private_dir,
+        'load_ssl': load_ssl,
+        'pem_source': pem_source,
+        'key_source': key_source,
+        'http_port': http_port,
+        'https_port': https_port,
+        'host_name': host_name,
+        'iptables_path': iptables_path,
+        'redis_host': redis_host,
+        'redis_port': redis_port,
+        'redis_pswd': redis_pswd,
+        'yum_repos_dir': yum_repos_dir,
+        'puias_priority': puias_priority,
+        'postfix': postfix,
+        'smtp_server_name': smtp_server_name,
+        'smtp_server_port': smtp_server_port,
+        'smtp_login': smtp_login,
+        'smtp_pswd': smtp_pswd
+    }
 
-        yum_repos_dir = _input_default('Insert the YUM repositories directory', '/etc/yum.repos.d')
-        puias_priority = _input_default('Insert the priority for the puias-unsupported repository', '19')
+    if _yes_no_input('Do you want to generate a configuration file from these data?', 'y'):
+        conf_path = _input_default('Specify the configuration file path', 'gen-user-data.conf')
 
-        conf_dict = {
-            'indico_inst_dir': indico_inst_dir,
-            'db_inst_dir': db_inst_dir,
-            'httpd_conf_dir': httpd_conf_dir,
-            'httpd_confd_dir': httpd_confd_dir,
-            'ssl_certs_dir': ssl_certs_dir,
-            'ssl_private_dir': ssl_private_dir,
-            'load_ssl': load_ssl,
-            'pem_source': pem_source,
-            'key_source': key_source,
-            'http_port': http_port,
-            'https_port': https_port,
-            'host_name': host_name,
-            'iptables_path': iptables_path,
-            'redis_host': redis_host,
-            'redis_port': redis_port,
-            'redis_pswd': redis_pswd,
-            'yum_repos_dir': yum_repos_dir,
-            'puias_priority': puias_priority,
-            'postfix': postfix,
-            'smtp_server_name': smtp_server_name,
-            'smtp_server_port': smtp_server_port,
-            'smtp_login': smtp_login,
-            'smtp_pswd': smtp_pswd
-        }
+        d = os.path.dirname(conf_path)
+        if (not os.path.exists(d)) & (d != ''):
+            os.makedirs(d)
 
-        if _yes_no_input('Do you want to generate a configuration file', 'y'):
-            conf_path = _input_default('Specify the configuration file path', 'gen-user-data.conf')
-
-            d = os.path.dirname(conf_path)
-            if (not os.path.exists(d)) & (d != ''):
-                os.makedirs(d)
-
-            with open(conf_path, 'w') as f:
-                dump(conf_dict, f, default_flow_style=False)
+        with open(conf_path, 'w') as f:
+            dump(conf_dict, f, default_flow_style=False)
 
     return conf_dict
 
 
 def _gen_file(rules_dict, in_path, out_path):
-    print(vrule)
-    print("Generating {0}".format(os.path.basename(in_path)))
+    print("Generating {0}... ".format(os.path.basename(in_path)), end="")
     with open(in_path, 'r') as fin:
         with open(out_path, 'w+') as fout:
             fout.write(fin.read().format(**rules_dict))
-    print(green("{0} generated".format(os.path.basename(in_path))))
+    print(cyan("done".format(os.path.basename(in_path))))
 
 
 def _gen_indico_httpd_conf(conf_dict):
@@ -281,16 +272,24 @@ def _gen_config_files(conf_dict):
 
 
 def main():
-    conf_dict = config()
-    mime_path = _input_default('Choose a path for the MIME file', "user-data")
-    print('---------- Starting MIME file generation ----------')
+    parser = argparse.ArgumentParser(description='Generate user-data file for cloud deployment of Indico.')
+    parser.add_argument('--config', metavar='FILE', help='use an existing config file (YAML)')
+    parser.add_argument('--output', metavar='FILE', default='user-data', help="output file (default: 'user-data')")
+
+    args = parser.parse_args()
+
+    if args.config:
+        with open(args.config, 'r') as f:
+            conf_dict = load(f)
+    else:
+        conf_dict = config()
+
     _gen_config_files(conf_dict)
-    os.system("./write-mime-multipart --output {0}".format(mime_path)
+    os.system("./write-mime-multipart --output {0}".format(args.output)
               + " {0}".format(os.path.join(conf_dir, 'user-data-script.sh'))
               + " {0}".format(os.path.join(conf_dir, 'cloud-config'))
               )
-    print(vrule)
-    print(green('--------------- MIME file generated ---------------'))
+    print(green("Wrote '{}'.".format(args.output)))
 
 
 if __name__ == '__main__':
